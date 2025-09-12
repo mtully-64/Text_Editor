b6bfc69bd11102ec7e8ae8dcd311358fd70493d4
Mark Tully <marktully2012@gmail.com>
2025-09-12 18:35:43 +0100

    Implemented the menu bar and menu items

--- src/GUI.java
+++ src/GUI.java
@@ -5,6 +5,9 @@ public class GUI{
     JFrame window;
     JTextArea textArea;
     JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu menuFile, menuEdit, menuFormat, menuColour;
+    JMenuItem iNew, iOpen, iSave, iSaveAs, iExit;
 
     public static void main(String[] args){
         new GUI();
@@ -13,7 +16,8 @@ public class GUI{
     public GUI(){
         createWindow();
         createTextArea();
-
+        createMenuBar();
+        createFileMenu();
         window.setVisible(true);
     }
 
@@ -30,5 +34,39 @@ public class GUI{
         scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         window.add(scrollPane);
+    }
+
+    public void createMenuBar(){
+        menuBar = new JMenuBar();
+        window.setJMenuBar(menuBar);
+
+        menuFile = new JMenu("File");
+        menuBar.add(menuFile);
+
+        menuEdit = new JMenu("Edit");
+        menuBar.add(menuEdit);
+
+        menuFormat = new JMenu("Format");
+        menuBar.add(menuFormat);
+
+        menuColour = new JMenu("Colour");
+        menuBar.add(menuColour);
+    }
+
+    public void createFileMenu(){
+        iNew = new JMenuItem("New");
+        menuFile.add(iNew);
+
+        iOpen = new JMenuItem("Open");
+        menuFile.add(iOpen);
+
+        iSave = new JMenuItem("Save");
+        menuFile.add(iSave);
+
+        iSaveAs = new JMenuItem("Save As");
+        menuFile.add(iSaveAs);
+
+        iExit = new JMenuItem("Exit");
+        menuFile.add(iExit);
     }
 }
